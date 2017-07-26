626ad9d4d684d381e7035f7f1a2bae29ae9625fb
Dmitry Lomako <dmitry.lomako@softeq.com>
2017-07-26 17:10:54 +0300

    *holy shit, all dom stuff was removed

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,111 +1,18 @@
-export function getDefaultInfo() {
-	let options = {};
-
-	//calculate scrollbar width
-	var scrollDiv = document.createElement("div");
-	scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -99px;';
-	document.body.appendChild(scrollDiv);
-	options.scrollbarSize = (scrollDiv.offsetWidth - scrollDiv.clientWidth) || 0;
-	document.body.removeChild(scrollDiv);
-	//end calculate scrollbar width
-
-	//detect features
-
-	//ie8 and below
-	options.oldIE = !!(document.all && !document.addEventListener);
-
-	//touch interface
-	options.touch = 'ontouchstart' in window;
-
-	//detect css3 support
-	let h = options;
-
-	h.nativeDialogSupport = !!document.createElement('dialog').showModal;
-	h.transition = styleSupport('transition');
-	h.transitionDuration = styleSupport('transitionDuration');
-	h.transform = styleSupport('transform');
-	h.animation = styleSupport('animation');
-
-	function styleSupport(prop) {
-		var vendorProp, supportedProp,
-			prefix, prefixes = ["Webkit", "Moz", "O", "ms"],
-			capProp = prop.charAt(0).toUpperCase() + prop.slice(1),// Capitalize first character of the prop to test vendor prefix
-			div = document.createElement("div");
-
-		document.body.insertBefore(div, null);
-
-		if (prop in div.style) {
-			supportedProp = prop;// Browser supports standard CSS property name
-			prefix = null;
-		} else {
-			for (var i = 0; i < prefixes.length; i++) {// Otherwise test support for vendor-prefixed property names
-				vendorProp = prefixes[i] + capProp;
-
-				if (vendorProp in div.style) {
-					prefix = prefixes[i];
-					break;
-				} else {
-					vendorProp = undefined;
-				}
-
-			}
-		}
-
-		var support = {
-			js: supportedProp || vendorProp,
-			css: writePrefixes(prop, prefix)
-		}
-
-		if (prop === 'transform') {//detect transform3d
-			if (div.style[support.js] !== undefined) {
-				div.style[support.js] = "translate3d(1px,1px,1px)";
-				var has3d = window.getComputedStyle(div)[support.js];
-			}
-			support['3d'] = (has3d !== undefined && has3d.length > 0 && has3d !== "none");
-		}
-
-		document.body.removeChild(div);
-		return support;
-	}
-
-	function writePrefixes(prop, prefix) {
-		//make prop camelCase
-		prop = prop.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-
-		if (prefix === null) {
-			return prop;
-		}
-
-		let ourPrefix;
-		switch (prefix) {
-			case 'Webkit':
-				ourPrefix = '-webkit-' + prop;
-				break;
-			case 'Moz':
-				ourPrefix = '-moz-' + prop;
-				break;
-			case 'ms':
-				ourPrefix = '-ms-' + prop;
-				break;
-			case 'O':
-				ourPrefix = '-o-' + prop;
-				break;
-		}
-		return ourPrefix;
-	}
-	//end of CSS3 support
-
-	return options
+export function isPlainObject(obj) {
+	return	typeof obj === 'object'
+								&& obj !== null
+								&& obj.constructor === Object
+								&& Object.prototype.toString.call(obj) === '[object Object]';
 }
 
 export function getItemFromDom(dom, selector) {
 	return dom.attr(selector) !== null ? dom : dom.find(`[${selector}]`);
 }
-export let defaults = {
+export const defaults = {
 	elem           : '',//(selector || dom\jQuery element) dom element for triggering modal
 	content        : undefined,//(string || function) content for modal
 	delayed        : true,//(boolean) Interesting option, that works only for selector and image types. When its true with selector type, dom element will not be touched until show, and will be returned to dom after hiding modal. When its true and type image, images will not be loading on initialization.
-	type           : '',//(html || selector || text || template) type of content, if selector used, whole element will be inserted in modal. Template similar to html, but template inserted without .njb__body tag(header/footer also not inserted), directly to .njb
+	type           : 'text',//(html || selector || text || template) type of content, if selector used, whole element will be inserted in modal. Template similar to html, but template inserted without .njb__body tag(header/footer also not inserted), directly to .njb
 	header         : undefined,//(html) html that will be added as modal header (for first slide)
 	footer         : undefined,//(html) html that will be added as modal footer (for first slide)
 	// we need quotes here because of ie8..
@@ -127,7 +34,7 @@ export let defaults = {
 	autoheight     : 'image',//(boolean || image) should we set maximum height of modal? if image is selected, only images will be autoheighted
 	autofocus      : true,//(boolean, selector) set focus to element, after modal is shown, also you may use autofocus attribute without this option
 	focusprevious  : true,//(boolean) focus previous modal window after closing curren modal (case when we open two or more modal windows)
-	title          : false,//(string || boolean false) title (usually for image)
+	title          : undefined,//(string || boolean false) title (usually for image)
 	title_attr     : 'title',//(string || boolean false) attribute from which we gather title for slide (used basically in images)
 
 	img            : 'ready',//(load || ready) we should wait until img will fully loaded or show as soon as size will be known (ready is useful for progressive images)
@@ -146,31 +53,15 @@ export let defaults = {
 	role           : 'dialog',//(dialog || alertdialog || boolean false) role that will be set to modal dialog https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_alertdialog_role
 	label          : false,//(string) add aria-label attribute to support screenreaders
 	labelledby     : false,//(id selector) add aria-labelledby attribute to support screenreaders
-	describedby    : false,//(id selector) add aria-describedby attribute to support screenreaders
+	describedby    : false//(id selector) add aria-describedby attribute to support screenreaders
+}
 
-	templates      : {
-		wrap          : '<div class="njb-wrap"><div class="njb-items"></div></div>',
-		backdrop      : '<div class="njb-backdrop"></div>',
-		modalOuter    : '<div class="njb-outer"></div>',
-		modal         : '<div class="njb"></div>',
-		body          : '<div class="njb__body" data-njb-body></div>',
-		header        : '<header class="njb__header" data-njb-header></header>',
-		footer        : '<footer class="njb__footer" data-njb-footer></footer>',
-		close         : '<button type="button" class="njb-ui__close" data-njb-close><span aria-hidden="true">×</span></button>',
-		focusCatcher  : '<span tabindex="0" class="njb-focus-catch" aria-hidden="true"></span>',
-
-		preloader     : '<div class="njb-preloader"><div class="njb-preloader__inner"><div class="njb-preloader__bar1"></div><div class="njb-preloader__bar2"></div><div class="njb-preloader__bar3"></div></div></div>',
-		ui            : '<div class="njb-ui"></div>',
-		title         : '<div class="njb-ui__title"><div class="njb-ui__title-inner" id="njb-title" data-njb-title></div></div>'//id in title used for accessibility
-	},
-
-	text           : {
-		_missedContent: 'njBox plugin: meow, put some content here...',//text for case, when slide have no content
-		preloader     : 'Loading...',//title on preloader element
-		imageError    : '<a href="%url%">This image</a> can not be loaded.',
-		close         : 'Close dialog',//title on close button
-		ok            : 'Ok',//text on 'ok' button when dialog modal(alert, prompt, confirm) or in any other custom type
-		cancel        : 'Cancel',//text on 'cancel' button when dialog modal(alert, prompt, confirm) or in any other custom type
-		placeholder   : ''//placeholder for prompt input
-	}
-};
+export const text = {
+	_missedContent: 'njBox plugin: meow, put some content here...',//text for case, when slide have no content
+	preloader     : 'Loading...',//title on preloader element
+	imageError    : '<a href="%url%">This image</a> can not be loaded.',
+	close         : 'Close dialog',//title on close button
+	ok            : 'Ok',//text on 'ok' button when dialog modal(alert, prompt, confirm) or in any other custom type
+	cancel        : 'Cancel',//text on 'cancel' button when dialog modal(alert, prompt, confirm) or in any other custom type
+	placeholder   : ''//placeholder for prompt input
+}
